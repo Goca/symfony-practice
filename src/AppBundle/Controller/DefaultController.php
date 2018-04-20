893ee6d271ff377f82d5690666109bbbe23461b3
Marija Budic <marija.budic@etondigital.com>
2018-04-20 14:04:26 +0200

    My first commit.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,8 +14,19 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('@App/Default/index.html.twig'); // , moze da ima i drugi argument ... array(), ne mora
+        
+        // return $this->render('default/index.html.twig'); // vraca difoltnu poce
+    }
+    
+    /**
+     * @Route("/user", name="user")
+     */
+     public function userAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('@App/Default/user.html.twig'); // , moze da ima i drugi argument ... array(), ne mora
+        
+        // return $this->render('default/index.html.twig'); // vraca difoltnu poce
     }
 }
