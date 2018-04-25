27ddf5023d13e3977d83a5c2274f109b05edd0f2
Marija Budic <marija.budic@etondigital.com>
2018-04-25 16:25:39 +0200

    Forms / practice

---
+++ src/AppBundle/Form/UserForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+
+class UserForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('ime', TextType::class)
+//            ->add('prezime', TextType::class, array('required' => false))
+//            ->add('opis', TextareaType::class, array('required' => false))
+            ->add('save', SubmitType::class, array('label' => 'Submit'))
+        ;
+    }   
+}
+
+
+//class WelcomeForm extends AbstractType
+//{
+//    public function buildForm(FormBuilderInterface $builder, array $options)
+//    {
+//        $builder
+//            ->add('Ime', TextType::class)
+//            ->add('save', SubmitType::class, array('label' => 'Submit'))
+//        ;
+//    }   
+//}
