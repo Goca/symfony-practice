041984af9c33e0bd208821a235ee58649a030095
Marija Budic <marija.budic@etondigital.com>
2018-05-14 10:05:51 +0200

    Doctrine/practice

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -159,6 +159,7 @@ class UserController extends Controller
     return $this->render('@App/User/list.html.twig', array('users' => $users));		       
   }
    
+  
   /**
    * @Route("/delete-user/{id}", name="app_user_delete")  
    */
